cce3303fcb8e6d238d71fd3c9a197a5f1fbbf755
Yap Wei Chun <wei.c@rocketbots.io>
2025-08-28 13:14:05 +0800

    feat: rename N8N to n8n for label on source

--- nodes/Respondio/constants/index.ts
+++ nodes/Respondio/constants/index.ts
@@ -75,7 +75,7 @@ const TRIGGER_SETTINGS_EVENT_SOURCES = {
     { name: 'Zapier', value: 'zapier' },
     { name: 'Bot', value: 'bot' },
     { name: 'Make', value: 'make' },
-    { name: 'N8N', value: 'n8n' },
+    { name: 'n8n', value: 'n8n' },
   ],
   CONVERSATION_OPENED: [
     { name: 'User', value: 'user' },
@@ -94,7 +94,7 @@ const TRIGGER_SETTINGS_EVENT_SOURCES = {
     { name: 'Bot', value: 'bot' },
     { name: 'Echo', value: 'echo' },
     { name: 'Make', value: 'make' },
-    { name: 'N8N', value: 'n8n' },
+    { name: 'n8n', value: 'n8n' },
   ]
 }
 
