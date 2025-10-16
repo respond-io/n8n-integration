e599696bb5f9f5af3c980d5ce677d8fc64b89e6e
Yap Wei Chun <wei.c@rocketbots.io>
2025-10-16 13:40:04 +0800

    fix: refactor lifecycles handler

--- nodes/Respondio/handlers/lifecycles/index.ts
+++ nodes/Respondio/handlers/lifecycles/index.ts
@@ -3,42 +3,52 @@ import { ACTION_NAMES } from "../../constants/actions/action_names";
 import { callDeveloperApi, constructIdentifier } from "../../utils";
 import { CreateContactResponse } from "../../types";
 
-const execute = async (
-  action: ACTION_NAMES,
-  executionContext: IExecuteFunctions,
-): Promise<INodeExecutionData[][] | NodeExecutionWithMetadata[][] | null> => {
-  // we only care about ASSIGN_OR_UNASSIGNED_CONVERSATION & OPEN_OR_CLOSE_CONVERSATION for the CONVERSATIONS operation
-  const allowedActions = [
-    ACTION_NAMES.REMOVE_CONTACT_LIFECYCLE,
-    ACTION_NAMES.UPDATE_CONTACT_LIFECYCLE,
-  ]
-  if (!allowedActions.includes(action)) return []
-
-  const identifier = constructIdentifier(executionContext);
-
-  if (action === ACTION_NAMES.REMOVE_CONTACT_LIFECYCLE) {
-    const response = await callDeveloperApi<CreateContactResponse>(executionContext, {
+const actionHandlers = {
+  [ACTION_NAMES.REMOVE_CONTACT_LIFECYCLE]: async (executionContext: IExecuteFunctions, _: number, identifier: string) => {
+    return callDeveloperApi<CreateContactResponse>(executionContext, {
       method: 'POST',
       path: `/contact/${identifier}/lifecycle/update`,
       body: { name: null }
     })
+  },
+  [ACTION_NAMES.UPDATE_CONTACT_LIFECYCLE]: async (executionContext: IExecuteFunctions, itemIndex: number, identifier: string) => {
+    const lifecycleName = executionContext.getNodeParameter('name', itemIndex, '') as string;
 
-    return [[{ json: response }]]
-  }
-
-  if (action === ACTION_NAMES.UPDATE_CONTACT_LIFECYCLE) {
-    const lifecycleName = executionContext.getNodeParameter('name', 0, '') as string;
-
-    const response = await callDeveloperApi<CreateContactResponse>(executionContext, {
+    return callDeveloperApi<CreateContactResponse>(executionContext, {
       method: 'POST',
       path: `/contact/${identifier}/lifecycle/update`,
       body: { name: lifecycleName },
     })
+  },
+}
 
-    return [[{ json: response }]]
+const ALLOWED_LIFECYCLE_ACTIONS = [
+  ACTION_NAMES.REMOVE_CONTACT_LIFECYCLE,
+  ACTION_NAMES.UPDATE_CONTACT_LIFECYCLE,
+] as const;
+
+type VALID_LIFECYCLE_ACTIONS = typeof ALLOWED_LIFECYCLE_ACTIONS[number];
+
+const execute = async (
+  action: VALID_LIFECYCLE_ACTIONS,
+  executionContext: IExecuteFunctions,
+): Promise<INodeExecutionData[][] | NodeExecutionWithMetadata[][] | null> => {
+  if (!ALLOWED_LIFECYCLE_ACTIONS.includes(action)) return []
+  const items = executionContext.getInputData();
+  const results: INodeExecutionData[] = [];
+  const handler = actionHandlers[action];
+
+  if (!handler) return [[{ json: { message: 'Action not handled' }, pairedItem: { item: 0 } }]]
+
+  for (let i = 0; i < items.length; i++) {
+    const identifier = constructIdentifier(executionContext, i);
+
+    const data = await handler(executionContext, i, identifier);
+
+    results.push({ json: data, pairedItem: { item: i } });
   }
 
-  return [[{ json: { message: 'Action not handled' } }]]
+  return [results];
 }
 
 export default { execute }
